2a66cb4c0819fca1b19b3ace3b6ae839709c5777
Seth Morabito <seth@loomcom.com>
2014-04-29 18:15:25 -0700

    Rename files

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ "$1" = "clean" ] ; then
     echo "Cleaning..."
     rm -f graph.png
     rm -f *.OLD
-    rm -f module
+    rm -f mfm
     rm -f graph.txt
     exit
 fi
@@ -13,7 +13,7 @@ if [ -f graph.png ] ; then
     mv -f graph.png graph.png.OLD
 fi
 
-iverilog -o module mfm_shift_register_tb.v mfm_shift_register.v &&
-    vvp module > graph.txt &&
+iverilog -o mfm mfm_tb.v mfm.v &&
+    vvp mfm > graph.txt &&
     gnuplot graph.gnuplot > graph.png &&
     open graph.png
