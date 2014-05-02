46d83738198dda19c885669d52edd3b044ae41c8
Seth Morabito <seth@loomcom.com>
2014-05-01 18:21:42 -0700

    Debounce MFM input

--- mfm.v
+++ mfm.v
@@ -1,4 +1,3 @@
-
 //
 // Digital PLL. The output, 'clk_5', will be synchronized with the
 // clock and data bits of the raw MFM. It is self-correcting for any
@@ -16,15 +15,17 @@ module MFM_DPLL (clk_50, raw_mfm, clk_5);
    reg [4:0] div_counter;   // Divide-by-ten counter
    reg [4:0] next_counter;  // How long is our next 1/2 cycle?
    reg first_sync;          // Have we ever found an MFM pulse?
+   reg mfm_state;           // State of the MFM input
 
    parameter COUNTER_VAL = 9;
 
    initial
      begin
-        div_counter = COUNTER_VAL;
-        next_counter = COUNTER_VAL;
+        div_counter = 0;
+        next_counter = 0;
         first_sync = 0;
         clk_5 = 0;
+        mfm_state = 0;
      end
 
    always @(posedge clk_50)
@@ -39,7 +40,7 @@ module MFM_DPLL (clk_50, raw_mfm, clk_5);
                begin
                   first_sync = 1;
                   div_counter = 0;
-                  // Take off one for the clock 50MHz cycle we just lost.
+                  // Take off one for the 50MHz cycle we just lost.
                   next_counter = COUNTER_VAL - 1;
                end
           end
@@ -60,19 +61,23 @@ module MFM_DPLL (clk_50, raw_mfm, clk_5);
    // If the MFM clock is earlier or later than we expect it, lengthen
    // or shorten the clock width to compensate and re-sync.
    //
-   always @(posedge raw_mfm)
-     if (div_counter > 5)
+   always @(posedge clk_50)
+     if (mfm_state != raw_mfm)
+       begin
+          mfm_state = raw_mfm;
+          if (div_counter > 5)
 
-       // MFM clock is early. Lengthen current 1/2 cycle. But since
-       // the raw MFM clock may be synced either to the rising edge or
-       // the falling edge of the 50 MHz clock, we must be sure that
-       // the 5MHz clock output is always lagging (never leading!) by
-       // at least 1 to 1.5 clk_50 cycles, for safety. Hence, the
-       // extra "1"
+            // MFM clock is early. Lengthen current 1/2 cycle. But since
+            // the raw MFM clock may be synced either to the rising edge or
+            // the falling edge of the 50 MHz clock, we must be sure that
+            // the 5MHz clock output is always lagging (never leading!) by
+            // at least 1 to 1.5 clk_50 cycles, for safety. Hence, the
+            // extra "1"
 
-       div_counter = div_counter + (COUNTER_VAL - div_counter + 1);
-     else if (div_counter > 0)
-       // MFM clock is late. Shortne next 1/2 cycle.
-       next_counter = COUNTER_VAL - div_counter;
+            div_counter = div_counter + (COUNTER_VAL - div_counter + 1);
+          else if (div_counter > 0)
+            // MFM clock is late. Shortne next 1/2 cycle.
+            next_counter = COUNTER_VAL - div_counter;
+       end
 
 endmodule
